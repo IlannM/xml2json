749e7ef27571e38a46037316ec346294c6b5998f
Ilan Mittelman <56805451+IlannM@users.noreply.github.com>
2024-02-26 00:13:18 +0200

    Update xml2json.cpp

--- xml2json.cpp
+++ xml2json.cpp
@@ -2,25 +2,32 @@
 // forked from https://github.com/Cheedoong/xml2json
 
 #include <xml2json.hpp>
+#include <fmt/format.h>
 #include <iostream>
 #include <sstream>
 #include <fstream>
-
-void convert(const std::istream &input) {
-    std::ostringstream oss;
-    oss << input.rdbuf();
-    const auto &json_str = xml2json(oss.str().data());
-    std::cout << json_str << std::endl;
-}
+#include <cstring>
 
 int main(int argc, char *argv[]) {
-    switch (argc) {
-    case 1:
-        convert(std::cin);
-        break;
-    case 2:
-        convert(std::ifstream(argv[1]));
-        break;
+    if (argc < 2) {
+        std::cout << "Please supply a XML file path.\n";
+        return 1;
+    } else if (argc > 3) {
+        std::cout << "Too many args.\n";
+        return 1;
     }
+    std::ifstream input(argv[1]);
+    input.seekg(0, std::ios::end);
+    const auto &size = input.tellg();
+    input.seekg (0, std::ios::beg);
+    char * memblock = new char [size];
+    input.read(memblock, size);
+    input.close();
+    const std::string &json_str = xml2json(memblock, argc == 3 ? (std::atoi(argv[2])) : 0);
+    const auto& len = strlen(argv[1]);
+    argv[1][len-3] = 0;
+    std::ofstream file(fmt::format("{}json", argv[1]));
+    file << json_str;
+    file.close();
     return 0;
 }
